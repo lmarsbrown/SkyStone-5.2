4e8cb425ff4630e9c1b6c7fc90b305a0b6718508
Logan(Maybe seth) <lmarsbrown@gmail.com>
2019-11-26 12:36:28 -0800

    Added Servos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/FieldCentric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/FieldCentric.java
@@ -2,7 +2,9 @@ package org.firstinspires.ftc.teamcode.OpModes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Robot.*;
@@ -24,6 +26,11 @@ public class FieldCentric extends OpMode {
     private DcMotor horizontal_extender;
     private DcMotor vertical_extender;
 
+    private Servo collector_arm;
+
+    private CRServo outer_collector;
+    private CRServo inner_collector;
+
     private double gp1_percent_pwr;
     private double gp2_percent_pwr;
 
@@ -40,6 +47,9 @@ public class FieldCentric extends OpMode {
         rightBack           = hardwareMap.get(DcMotor.class, "right_back");
         horizontal_extender = hardwareMap.get(DcMotor.class, "horizontal_ext");
         vertical_extender   = hardwareMap.get(DcMotor.class, "vertical_ext");
+        collector_arm       = hardwareMap.get(Servo.class, "collector_arm");
+        outer_collector     = hardwareMap.get(CRServo.class, "outer_collector");
+        inner_collector     = hardwareMap.get(CRServo.class, "inner_collector");
 
         leftFront.setDirection(DcMotor.Direction.FORWARD);
         rightFront.setDirection(DcMotor.Direction.REVERSE);
@@ -50,11 +60,15 @@ public class FieldCentric extends OpMode {
         rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        vertical_extender.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        horizontal_extender.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         rowboat = new Robot_Localizer(leftBack,rightFront,rightBack,0.958);
         control = new Robot_Controller(rightFront,leftFront,rightBack,leftBack,rowboat);
 
         going_to_pt = false;
+
+        collector_arm.setPosition(0.403);
     }
 
     @Override
@@ -72,9 +86,11 @@ public class FieldCentric extends OpMode {
         robot_vector = new Transform(gamepad1.left_stick_x,gamepad1.left_stick_y,gamepad1.right_stick_x);
 
         if(gamepad1.left_bumper) gp1_percent_pwr = 0.25;
+        else if (gamepad1.right_bumper) gp1_percent_pwr = 0.6;
         else gp1_percent_pwr = 1;
 
         if(gamepad2.left_bumper) gp2_percent_pwr = 0.25;
+        else if (gamepad2.right_bumper) gp2_percent_pwr = 0.6;
         else gp2_percent_pwr = 1;
 
         if(gamepad1.y) saved_robot_pos = rowboat.pos.clone();
@@ -89,18 +105,24 @@ public class FieldCentric extends OpMode {
             control.clearGoto();
         }
 
-        // Uncomment the next line for Field-Centric Driving
         robot_vector.rotate(new Transform(0, 0, 0), -rowboat.pos.r);
 
         if(!going_to_pt) control.setVec(robot_vector, gp1_percent_pwr);
 
-        horizontal_extender.setPower(-gamepad2.left_stick_y * gp2_percent_pwr);
+        if(gamepad2.left_trigger != 0)       horizontal_extender.setPower(-gamepad2.left_trigger  * gp2_percent_pwr);
+        else if(gamepad2.right_trigger != 0) horizontal_extender.setPower( gamepad2.right_trigger * gp2_percent_pwr);
 
-        vertical_extender.setPower(gamepad2.right_stick_y * gp2_percent_pwr);
+        vertical_extender.setPower(-gamepad2.left_stick_y * gp2_percent_pwr);
 
-        telemetry.addData("r",rowboat.pos.r);
-        telemetry.addData("x",rowboat.pos.x);
-        telemetry.addData("y",rowboat.pos.y);
+        inner_collector.setPower(gamepad2.right_stick_y * gp2_percent_pwr);
+        outer_collector.setPower(gamepad2.right_stick_y * gp2_percent_pwr);
+
+        if(gamepad2.dpad_up)        collector_arm.setPosition(0.7 * gp2_percent_pwr);
+        else if(gamepad2.dpad_down) collector_arm.setPosition(0.403 * gp2_percent_pwr);
+
+        telemetry.addData("r", rowboat.pos.r);
+        telemetry.addData("x", rowboat.pos.x);
+        telemetry.addData("y", rowboat.pos.y);
         telemetry.update();
     }
 
